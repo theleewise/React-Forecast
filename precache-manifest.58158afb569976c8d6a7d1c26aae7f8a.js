32e150e9a68fdbeec49290e445f68e1f4b14a714
Lee Wise <theleewise@gmail.com>
2019-10-22 21:44:24 -0500

    Updates

---
+++ precache-manifest.58158afb569976c8d6a7d1c26aae7f8a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "35eb268dd7e8dfe694b55fec314a0b10",
+    "url": "/react-forecast/static/media/rainy-2.35eb268d.svg"
+  },
+  {
+    "revision": "c54223e30b59a60bfb1b",
+    "url": "/react-forecast/static/js/main.4c1d0dfd.chunk.js"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/react-forecast/static/media/thunder.7142c78b.svg"
+  },
+  {
+    "revision": "80a5d865c63ba642a927daba54438305",
+    "url": "/react-forecast/static/media/cloudy-day-1.80a5d865.svg"
+  },
+  {
+    "revision": "b931d84d75a673fdd8ae1134fefd98ba",
+    "url": "/react-forecast/static/media/cloudy-day-2.b931d84d.svg"
+  },
+  {
+    "revision": "109368f632c5dafab6cbf47ec386f8d0",
+    "url": "/react-forecast/static/media/cloudy-day-3.109368f6.svg"
+  },
+  {
+    "revision": "7e65840bf2249d8f7ba19fc029438545",
+    "url": "/react-forecast/static/media/cloudy-night-1.7e65840b.svg"
+  },
+  {
+    "revision": "ef1c6aa1ca72ea2157736318d24ede62",
+    "url": "/react-forecast/static/media/cloudy-night-2.ef1c6aa1.svg"
+  },
+  {
+    "revision": "bf91ac0f97cf729862dfaeb8fc25e037",
+    "url": "/react-forecast/static/media/cloudy-night-3.bf91ac0f.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/react-forecast/static/media/cloudy.ad789581.svg"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/react-forecast/static/media/day.7f93fbdb.svg"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/react-forecast/static/media/night.a3f3f38d.svg"
+  },
+  {
+    "revision": "f134a51c3c0a49f33fecc7a201a4e075",
+    "url": "/react-forecast/static/media/rainy-1.f134a51c.svg"
+  },
+  {
+    "revision": "c16a41bda854a06a503f",
+    "url": "/react-forecast/static/js/runtime~main.f3de8ea7.js"
+  },
+  {
+    "revision": "c17e7a5d3d1b7d702bc8c640a36f68c6",
+    "url": "/react-forecast/static/media/rainy-3.c17e7a5d.svg"
+  },
+  {
+    "revision": "c8cdb21695a664b4dbffeb1fc9631510",
+    "url": "/react-forecast/static/media/rainy-4.c8cdb216.svg"
+  },
+  {
+    "revision": "2bd236411dc612e871d9c314239c3fa7",
+    "url": "/react-forecast/static/media/rainy-5.2bd23641.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/react-forecast/static/media/rainy-6.d235ab65.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/react-forecast/static/media/rainy-7.c915395f.svg"
+  },
+  {
+    "revision": "f78cba60ad28e30ea060d424f114670a",
+    "url": "/react-forecast/static/media/snowy-1.f78cba60.svg"
+  },
+  {
+    "revision": "d103c77cf7f40bc52812fe945ddd3cfa",
+    "url": "/react-forecast/static/media/snowy-2.d103c77c.svg"
+  },
+  {
+    "revision": "b6eea2cf5e8a5ab5f57405addab863ae",
+    "url": "/react-forecast/static/media/snowy-3.b6eea2cf.svg"
+  },
+  {
+    "revision": "474b6da7e78dc3dadc11766d80f7eaca",
+    "url": "/react-forecast/static/media/snowy-4.474b6da7.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/react-forecast/static/media/snowy-5.3def7c9f.svg"
+  },
+  {
+    "revision": "67d63a14dbbef1d431f1851da197e0ae",
+    "url": "/react-forecast/static/media/snowy-6.67d63a14.svg"
+  },
+  {
+    "revision": "59f0f18c291d613e68e1",
+    "url": "/react-forecast/static/js/2.74c83060.chunk.js"
+  },
+  {
+    "revision": "3a791b53320b1650f9f006c54af29d04",
+    "url": "/react-forecast/index.html"
+  }
+];
